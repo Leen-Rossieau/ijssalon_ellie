7c73e67fb781d1011e8f4b42e807a634bfacf104
Leen Rossieau <lrossieau@verebus.nl>
2026-03-10 11:50:49 +0100

    bestand tijdelijk.py gecodeerd

--- helper.py
+++ helper.py
@@ -1,7 +1,22 @@
-tekst="header"
-lengte=len(tekst) + 4
-print()
-print(lengte * "*")
-print(f"* {tekst} *")
-print(lengte * "*")
-print()
+# tekst="header"
+# lengte=len(tekst) + 4
+# print()
+# print(lengte * "*")
+# print(f"* {tekst} *")
+# print(lengte * "*")
+# print()
+
+# woord="chocolade"
+# aantal= len(woord)
+# print(f"Er zitten {aantal} letters in het woord {woord}.")
+
+# woorden="Mijn favoriete smaak is aardbei".split(" ")
+# print(woorden)
+
+# woorden="Mijn favoriete smaak is aardbei".index("smaak")
+# print(woorden)
+
+# woorden="Mijn favoriete smaak is aardbei"
+# smaak="citroen"
+# print(f"Mijn favoriete smaak is {smaak}")
+
